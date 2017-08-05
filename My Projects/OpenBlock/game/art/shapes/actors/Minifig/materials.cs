45505be39a7df33256b586982402872239f436b5
DShiznit <gm240414@gmail.com>
2017-08-05 12:03:11 -0400

    New Minifig
    I'm committing a new, improved minifig model which, while not perfect, should be suitable for placeholder use until I figure out how to fix this model. The location of the minifig has been changed to art/shapes/actors/Minifig. Some temp files have also been changed, though they shouldn't be consequential.

---
+++ My Projects/OpenBlock/game/art/shapes/actors/Minifig/materials.cs
@@ -0,0 +1,82 @@
+
+singleton Material(minifig_matLegR)
+{
+   mapTo = "matLegR";
+   diffuseMap[0] = "base.figLimb";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matLegL)
+{
+   mapTo = "matLegL";
+   diffuseMap[0] = "base.figLimb";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matHip)
+{
+   mapTo = "matHip";
+   diffuseColor[3] = "White";
+   diffuseMap[0] = "base.figHead";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matHead)
+{
+   mapTo = "matHead";
+   diffuseMap[0] = "base.figHead";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matHandR)
+{
+   mapTo = "matHandR";
+   diffuseMap[0] = "base.figLimb";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matHandL)
+{
+   mapTo = "matHandL";
+   diffuseMap[0] = "base.figLimb";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matChest)
+{
+   mapTo = "matChest";
+   diffuseMap[0] = "base.figTorso";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matArmR)
+{
+   mapTo = "matArmR";
+   diffuseMap[0] = "base.figLimb";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(minifig_matArmL)
+{
+   mapTo = "matArmL";
+   diffuseMap[0] = "base.figLimb";
+   specular[0] = "0 0 0 1";
+   specularPower[0] = "1";
+   translucentBlendOp = "None";
+};
---
+++ My Projects/OpenBlock/game/art/shapes/actors/Minifig/minifig.cs
@@ -0,0 +1,8 @@
+
+singleton TSShapeConstructor(MinifigDae)
+{
+   baseShape = "./minifig.dae";
+   lodType = "TrailingNumber";
+   singleDetailSize = "0";
+   loadLights = "0";
+};
